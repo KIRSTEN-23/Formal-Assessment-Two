88b0a3de749906e13c711be32b1394b865ecec76
KIRSTEN-23 <231296@virtualwindow.co.za>
2023-08-09 23:53:54 +0200

    add arrays

---
+++ scripts/main.js
@@ -0,0 +1,60 @@
+// ------------------------------------------------------------------------
+// Trips Array
+// ------------------------------------------------------------------------
+
+const arrTrips = [
+    {
+      destination: "Barcelona",
+      price: 8000,
+      region: "Mediterranean",
+      image: "Mediterranean-Barcelona.png",
+      type: "Single Destination",
+      departure: "2023-03-25",
+      duration: "5 Days"
+    },
+    {
+      destination: "Genoa",
+      price: 8000,
+      region: "Mediterranean",
+      image: "Mediterranean-Genoa.png",
+      type: "Single Destination",
+      departure: "2023-03-30",
+      duration: "5 Days"
+    },
+    {
+      destination: "Marseilles",
+      price: 8000,
+      region: "Mediterranean",
+      image: "Mediterranean-Marseilles.png",
+      type: "Single Destination",
+      departure: "2023-04-04",
+      duration: "5 Days"
+    },
+    {
+      destination: "Messina",
+      price: 8000,
+      region: "Mediterranean",
+      image: "Mediterranean-Messina.png",
+      type: "Single Destination",
+      departure: "2023-04-09",
+      duration: "5 Days"
+    },
+    {
+      destination: "Naples",
+      price: 8000,
+      region: "Mediterranean",
+      image: "Mediterranean-Naples.png",
+      type: "Single Destination",
+      departure: "2023-04-14",
+      duration: "5 Days"
+    },
+    {
+        destination: "Valletta",
+        price: 8000,
+        region: "Mediterranean",
+        image: "Mediterranean-Valletta.png",
+        type: "Single Destination",
+        departure: "2023-04-19",
+        duration: "5 Days"
+      },
+  ];
